8923f3f8015a885071c945e83c5373dae099c85a
vande1me <mevanderkuur76>
2016-05-23 08:25:21 -0400

    setup ints

--- Main.cpp
+++ Main.cpp
@@ -2,3 +2,17 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+int randNum(int n);
+void pick_a_question();
+int select_next_available(int n);
+
+bool quest_ans[4];
+int questions;
+
+int main(){
+		int score = 0;
+		int count = 0;
+		int questions;
+		int r, i, n;
+		char answer;
