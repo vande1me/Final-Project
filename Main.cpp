c4f0ac5eae5e8ca68117c482402e8a2aeb701a58
vande1me <mevanderkuur76>
2016-05-24 07:46:57 -0400

    made first question

--- Main.cpp
+++ Main.cpp
@@ -1,3 +1,4 @@
+#include <ctime>
 #include <cstdlib>
 #include <iostream>
 #include <string>
@@ -16,3 +17,38 @@ int main(){
 		int questions;
 		int r, i, n;
 		char answer;
+		
+		srand(time(0)); // set seed for randomizing
+			cout << "How many questions would you like to answer? ";
+			cout << endl;
+		for (i = 1; i <= questions; i++)	
+			pick_a_question();
+
+		switch (r)
+			
+		{
+			
+		case 1:
+				cout << "What fruit is red? " << endl;
+				cout << "A) Banana" << endl;
+				cout << "B)/>/> Strawberry" << endl;
+				cout << "C) Kiwi" << endl;
+				cout << "D) Pineapple" << endl;
+				cin >> answer;
+
+				questions--;
+				if (answer == 'b' || answer == 'B')
+				{
+					cout << "Correct" << endl;
+					score++;
+				}
+				else
+	
+				{
+						cout << "Incorrect" << endl;
+						cout << "The correct answer is B" << endl;
+				}
+					break;
+
+	
+
